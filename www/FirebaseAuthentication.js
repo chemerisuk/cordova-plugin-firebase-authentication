7248e0c2869349daedc92bfad65e91115a69e75a
Maksim Chemerisuk <chemerisuk@gmail.com>
2017-07-17 21:32:30 +0300

    bugfix

--- www/FirebaseAuthentication.js
+++ www/FirebaseAuthentication.js
@@ -2,7 +2,7 @@ var exec = require("cordova/exec");
 var PLUGIN_NAME = "FirebaseAuthentication";
 
 module.exports = {
-    getIdToken: function(forceRefresh) {
+    getIdToken: function(forceRefresh, success, error) {
         exec(success, error, PLUGIN_NAME, "getIdToken", [forceRefresh]);
     },
     signInWithEmailAndPassword: function(email, password, success, error) {
